61bc55d78b76e84212cbc40a5737d4570e19c9d6
kartsridhar <contactkarthik34@gmail.com>
2019-06-24 02:05:00 +0100

    finished responsive JS

--- js/functions.js
+++ js/functions.js
@@ -28,6 +28,19 @@ const navSlide = () => {
   burger.addEventListener('click', () => {
     // Toggle navigation
     nav.classList.toggle('nav-active');
+
+    //Animate links
+    navLinks.forEach((link, index) => {
+      if(link.style.animation) {
+        link.style.animation = '';
+      }
+      else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.4}s`;
+      }
+    });
+
+    //Burger animation
+    burger.classList.toggle('toggle');
   });
 }
 
