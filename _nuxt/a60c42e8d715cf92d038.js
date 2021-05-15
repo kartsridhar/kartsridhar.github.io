d927c8e8053ec7d10f277cb6ecffe8f7baaf0ae0
kartsridhar <contactkarthik34@gmail.com>
2021-05-15 01:30:53 +0100

    removing for now

--- _nuxt/a60c42e8d715cf92d038.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{165:function(n,t,e){"use strict";e.r(t);var r={data:function(){return{}}},o=e(19),component=Object(o.a)(r,(function(){var n=this.$createElement;return(this._self._c||n)("div",{},[this._v("\n    Work in progress...\n")])}),[],!1,null,null,null);t.default=component.exports}}]);
